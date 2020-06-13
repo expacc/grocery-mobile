29e0b954add0dad6af46d41586b42b32da4d97c9
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-06-14 01:13:53 +0700

    Change Languages

--- lib/src/models/language.dart
+++ lib/src/models/language.dart
@@ -5,7 +5,8 @@ class Language {
   String flag;
   bool selected;
 
-  Language(this.code, this.englishName, this.localName, this.flag, {this.selected = false});
+  Language(this.code, this.englishName, this.localName, this.flag,
+      {this.selected = false});
 }
 
 class LanguagesList {
@@ -13,13 +14,54 @@ class LanguagesList {
 
   LanguagesList() {
     this._languages = [
-      new Language("en", "English", "English", "assets/img/united-states-of-america.png"),
-      new Language("ar", "Arabic", "العربية", "assets/img/united-arab-emirates.png"),
-      new Language("es", "Spanish", "Spana", "assets/img/spain.png"),
-      new Language("fr", "French (France)", "Français - France", "assets/img/france.png"),
-      new Language("fr", "French (Canada)", "Français - Canadien", "assets/img/canada.png"),
-      new Language("pt", "Brazilian", "Brazilian", "assets/img/brazil.png"),
-      new Language("ko", "Korean", "Korean", "assets/img/united-states-of-america.png"),
+      new Language(
+        "id",
+        "Bahasa",
+        "Indonesia",
+        "assets/img/indonesia.png",
+      ),
+      new Language(
+        "en",
+        "English",
+        "English",
+        "assets/img/united-states-of-america.png",
+      ),
+      // new Language(
+      //   "ar",
+      //   "Arabic",
+      //   "العربية",
+      //   "assets/img/united-arab-emirates.png",
+      // ),
+      // new Language(
+      //   "es",
+      //   "Spanish",
+      //   "Spana",
+      //   "assets/img/spain.png",
+      // ),
+      // new Language(
+      //   "fr",
+      //   "French (France)",
+      //   "Français - France",
+      //   "assets/img/france.png",
+      // ),
+      // new Language(
+      //   "fr",
+      //   "French (Canada)",
+      //   "Français - Canadien",
+      //   "assets/img/canada.png",
+      // ),
+      // new Language(
+      //   "pt",
+      //   "Brazilian",
+      //   "Brazilian",
+      //   "assets/img/brazil.png",
+      // ),
+      // new Language(
+      //   "ko",
+      //   "Korean",
+      //   "Korean",
+      //   "assets/img/united-states-of-america.png",
+      // ),
     ];
   }
 
